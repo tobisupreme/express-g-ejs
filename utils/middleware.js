fb7b3e595f2ef029d44769a7f0949defa103a2ad
Tobi Balogun <tobi.balo@gmail.com>
2022-11-17 21:06:36 +0100

    Initial commit

---
+++ utils/middleware.js
@@ -0,0 +1,40 @@
+const logger = require('./logger')
+
+const requestLogger = (req, res, next) => {
+  logger.info('Method:', req.method)
+  logger.info('Path:  ', req.path)
+  logger.info('Body:  ', req.body)
+  logger.info('-----')
+  next()
+}
+
+const unknownEndpoint = (req, res, next) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+  next()
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({
+      error: 'invalid token',
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({
+      error: 'token expired',
+    })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+}
